450bef9f366d5091c5a465f1495cd300472938cb
Bruno dos Santos <projetointegrador792@gmail.com>
2022-07-10 16:17:11 -0300

    netflix-clone Homepage fix-usestate

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -9,11 +9,12 @@ interface Props {
     netflixOriginals?: Movie[];
 }
 
-const Banner = ({ netflixOriginals = [] }: Props) => {
+const Banner = ({ netflixOriginals}: Props) => {
     const [movie, setMovie] = useState<Movie | null>(null);
 
     useEffect(() => {
-        setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals!.length)])
+        const currentMovie = netflixOriginals!
+        setMovie(currentMovie[Math.floor(Math.random() * currentMovie.length)])
     }, [])
 
     return (
